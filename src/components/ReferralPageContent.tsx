515a309e909cd0359b52b6dd6f0733821a73a99a
Berny <berny.ust@gmail.com>
2025-05-28 16:03:11 +0200

    fix lint

--- src/components/ReferralPageContent.tsx
+++ src/components/ReferralPageContent.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect } from 'react';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
